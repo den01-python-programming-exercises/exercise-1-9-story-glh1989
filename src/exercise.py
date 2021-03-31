1d840506cb3c8bb2980e2a0ab812287089be096a
gh1989 <hughesg368@hwbcymru.net>
2021-03-31 10:54:33 +0000

    attempt solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,11 @@
 def main():
-    #write your code below this line
+    first = input('I will tell you a story, but I need some information first. What is the main character called?')
+
+    second = input('What is their job?')
+
+    print("Here is the story:")
+
+    print("Once upon a time there was " + first + ", who was a " + second + ". On the way to work, " + first + " reflected on life. Perhaps " + first + " will not be a " + second + " forever." )
 
 if __name__ == '__main__':
     main()
